2d94b8b0303af5e866348ea0aa6cfe86674e9af6
Техникум <@LAPTOP-0062G1LB>
2022-02-05 12:33:40 +0300

    Добавьте файлы проекта.

---
+++ Figure.cs
@@ -0,0 +1,70 @@
+﻿internal class Figure
+{
+    private string template;
+    private ConsoleColor color;
+    private char symbol;
+    private char background;
+
+    public Figure(string template, ConsoleColor color, char symbol, char background)
+    {
+        this.template = template;
+        this.color = color;
+        this.symbol = symbol;
+        this.background = background;
+        format();
+    }
+
+    public ConsoleColor Color
+    {
+        get { return color; }
+    }
+    private void format()
+    {
+        template = template
+        .Replace(' ', background)
+        .Replace('*', symbol)
+        .Replace("\r\n", "\n")
+        .Trim();
+    }
+    public override string ToString()
+    {
+        return template;
+    }
+    public Figure RotateLeft()
+    {
+        string res = "";
+        string[] lines = template.Split("\n");
+        for (int i = lines[0].Length - 1; i >= 0; i--)
+        {
+            foreach (string line in lines)
+                res += line[i];
+            res += "\n";
+        }
+        template = res.Trim();
+        return this;
+    }
+    public Figure RotateRight()
+    {
+        return this.RotateLeft().RotateLeft().RotateLeft();
+    }
+    public Figure HorizontalFlip()
+    {
+        string[] lines = template.Split('\n');
+        string res = "";
+        foreach (string line in lines)
+        {
+            char[] chars = line.ToArray();
+            Array.Reverse(chars);
+            foreach (char c in chars) res += c;
+            res += "\n";
+        }
+        template = res.Trim();
+        return this;
+    }
+    public Figure VerticalFlip()
+    {
+        return this.RotateLeft()
+            .HorizontalFlip()
+            .RotateRight();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿//1. Вывод фигур по номеру
+//2. Поворот фигур
+//3. Вертикальное/горизонтальное отображение
+
+Dictionary<int, string> figures = new Dictionary<int, string>()
+{
+    {1, @"
+  *
+***
+ * 
+" },
+    {2, @"
+****
+  * 
+" },
+    {3, @"
+  *
+***
+*  " }
+};
+
+string template = figures[2];
+ConsoleColor color = ConsoleColor.Blue;
+char background = '·';
+char symbol = '▓';
+
+Figure f = new Figure(template, color, symbol, background);
+ToConsole(f);
+bool isContinue = true;
+while(isContinue)
+{
+    ConsoleKey key = Console.ReadKey().Key;
+    if (key == ConsoleKey.Escape) isContinue = false;
+    else ToConsole(transform(f, key));
+}
+
+Figure transform(Figure f, ConsoleKey key)
+{
+    switch(key)
+    {
+        case ConsoleKey.R: return f.RotateRight();
+        case ConsoleKey.L: return f.RotateLeft();
+        case ConsoleKey.H: return f.HorizontalFlip();
+        case ConsoleKey.V: return f.VerticalFlip();
+        default: return f;
+    }
+}
+void ToConsole(Figure figure)
+{
+    Console.Clear();
+    Console.ForegroundColor = figure.Color;
+    Console.WriteLine(figure);
+    Console.ResetColor();
+}
+
